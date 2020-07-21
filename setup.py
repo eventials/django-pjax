ca82203361f54057b7b18e5af7b36559284f01bc
Guilherme Emilio Raduenz <skrater@gmail.com>
2020-07-20 23:39:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='django-pjax',
-    version='1.5',
+    version='1.7',
     description='Django-PJAX: The Django helper for jQuery-PJAX.',
     license='BSD',
     long_description=read('README.rst'),
